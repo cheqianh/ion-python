0ff23ff6fddac7a08b6764137f5c41483f1080f3
Eric Chen <cheqianh@amazon.com>
2020-12-03 12:36:37 -0800

    c ext

---
+++ amazon/ion/_ioncmodule.h
@@ -0,0 +1,14 @@
+#ifndef _IONCMODULE_H_
+#define _IONCMODULE_H_
+
+#include "structmember.h"
+#include "decimal128.h"
+#include "ion.h"
+
+PyObject* ionc_init_module(void);
+PyObject* helloworld(PyObject* self, PyObject *args);
+int _ionc_write(PyObject* obj, PyObject* binary, ION_STREAM* f_ion_stream);
+PyObject* ionc_read(PyObject* self, PyObject *args, PyObject *kwds);
+iERR ionc_read_all(hREADER hreader, PyObject* container, BOOL in_struct, BOOL emit_bare_values);
+
+#endif
