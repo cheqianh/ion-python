77bc335621e1781081feb6d33f4b56195dcc23f5
Eric Chen <cheqianh@amazon.com>
2021-04-16 18:44:06 -0700

    test with mac

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def run_setup():
 
     setup(
         name='amazon.ion',
-        version='0.7.96',
+        version='0.7.97',
         description='A Python implementation of Amazon Ion.',
         url='http://github.com/amzn/ion-python',
         author='Amazon Ion Team',
